face1bbf92f52431fd7571d3f0f53084549231b6
ZZyuandaren <97601618+ZZyuandaren@users.noreply.github.com>
2022-01-17 20:53:32 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,23 +1,71 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include<string.h>
-
+#include<windows.h>
 int main()
 {
-	int i;
-	int n, ret=1, sum=0;
-	for (i = 1; i <= 10; i++)
-	{
-		ret = 1;
-		for (n = 1; n <=i; n++)
-		{
-			ret = ret * n;
-		}
-		sum = sum + ret;
+	char arr1[] = "welcome to bit!!!!!!";
+	char arr2[] = "####################";
+	int left = 0;
+	int right = sizeof(arr1) / sizeof(arr1[0]) - 2;
+	while(left<=right)
+	{ 
+		arr2[left] = arr1[left];
+		arr2[right] = arr1[right];
+		printf("%s\n", arr2);
+		//停一秒
+		Sleep(1000);
+		right--;
+		left++;
 	}
-	printf("%d", sum);
 	return 0;
+
 }
+//int main()//   二分法
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10,11 };
+//	int k = 7;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int left = 0;
+//	int right = sz - 1;
+//	while(left<=right)
+//	{
+//		int mid = (left + right) / 2;
+//		if (arr[mid] > k)
+//		{
+//			right = mid + 1;
+//		}
+//		else if (arr[mid] < k)
+//		{
+//			left = mid - 1;
+//		}
+//		else
+//		{
+//			printf("找到了，下标是：%d", mid);
+//				break;
+//		}
+//	}
+//	if (left > right)
+//		printf("找不到\n");
+//	
+//	return 0;
+//}
+//int main()//阶乘题
+//{
+//	int i;
+//	int n, ret=1, sum=0;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		ret = 1;
+//		for (n = 1; n <=i; n++)
+//		{
+//			ret = ret * n;
+//		}
+//		sum = sum + ret;
+//	}
+//	printf("%d", sum);
+//	return 0;
+//}
 //int main()//求n的阶乘
 //{
 //	int n = 0;
