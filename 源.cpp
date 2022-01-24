3110fd2eebe2fd612ac821b7dd87a6d4fbcf6917
ZZyuandaren <97601618+ZZyuandaren@users.noreply.github.com>
2022-01-24 21:32:42 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -3,41 +3,66 @@
 #include<string.h>
 #include<windows.h>
 
-int search(int arr[], int k,int sz)
-{
-	int left = 0;
-	int right = sz;
-	while (left<=right)
-	{
-		int mid=0;
-		mid = (left + right) / 2;
-		if (arr[mid] < k)
-		{
-			left = mid+1;
-		}
-		else if (arr[mid] > k)
-		{
-			right = mid-1;
-		}
-		else
-		{
-			return mid;
-		}
-	}
-	return -1;
-}
 int main()
 {
-	int arr[] = { 1,2,3,4,5,6,7,8,9 ,10};
-	int k=7;
-	int sz = sizeof(arr) / sizeof(arr[0]) - 1;
-	int ret =search(arr, k,sz);
-	if (ret == -1)
-		printf("找不到");
-	else
-		printf("找到了，下标是：%d\n", ret);
+
 	return 0;
 }
+
+//void Add(int*p)
+//{
+//	*p = *p + 1;
+//}
+//
+//int main()
+//{
+//	int num = 0;
+//	Add(&num);
+//	printf("num=%d\n", num);
+//	Add(&num);
+//	printf("num=%d\n", num);
+//	Add(&num);
+//	printf("num=%d\n", num);
+//	Add(&num);
+//	printf("num=%d\n", num);
+//	return 0;
+//}
+// 
+//int search(int arr[], int k,int sz)//二分法查找
+//{
+//	int left = 0;
+//	int right = sz;
+//	while (left<=right)
+//	{
+//		int mid=0;
+//		mid = (left + right) / 2;
+//		if (arr[mid] < k)
+//		{
+//			left = mid+1;
+//		}
+//		else if (arr[mid] > k)
+//		{
+//			right = mid-1;
+//		}
+//		else
+//		{
+//			return mid;
+//		}
+//	}
+//	return -1;
+//}
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9 ,10};
+//	int k=7;
+//	int sz = sizeof(arr) / sizeof(arr[0]) - 1;
+//	int ret =search(arr, k,sz);
+//	if (ret == -1)
+//		printf("找不到");
+//	else
+//		printf("找到了，下标是：%d\n", ret);
+//	return 0;
+//}
 //int pd(int x)
 //{
 //	if((x%4==0&&x%100!=0)||(x%400==0))
