edbfc1f3ca404b687fc909bc4444373f8b676dd3
ZZyuandaren <97601618+ZZyuandaren@users.noreply.github.com>
2022-01-27 21:40:40 +0800

    Add files via upload

--- text.2.cpp
+++ text.2.cpp
@@ -1,25 +1,77 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<string.h>
 
- void hanoi(int n, char a, char b, char c)       //这里代表将a柱子上的盘子借助b柱子移动到c柱子
-   {  if (1 == n)                                //如果是一个盘子直接将a柱子上的盘子移动到c
-          {
-	             printf("%c-->%c\n", a, c);
-	           }
-        else
-            {
-	              hanoi(n - 1, a, c, b);         //将a柱子上n-1个盘子借助c柱子，移动到b柱子
-	              printf("%c-->%c\n", a, c);     //再直接将a柱子上的最后一个盘子移动到c
-	              hanoi(n - 1, b, a, c);         //然后将b柱子上的n-1个盘子借助a移动到c
-	           }
-   }
- int main()
-   {  int  n;
-       printf("Input the number of diskes:");
-       scanf("%d", &n);
-       hanoi(n, 'A', 'B', 'C');
-       return 0;
-   }
+int main()//数组的学习和应用
+{
+	int arr[3][4] = { {1,2,3},{4,5 },{7} };
+	int i;
+	for (i = 0; i < 3; i++)
+	{
+		int j;
+		for (j = 0; j < 4; j++)
+		{
+			printf("&arr[%d][%d]= %p\n",i,j, &arr[i][j]);
+		}
+		
+	}
+	return 0;
+}
+//int main()//数组的学习和应用
+//{
+//	int arr[3][4] = { {1,2,3},{4,5 },{7} };
+//	int i;
+//	for (i = 0; i < 3; i++)
+//	{
+//		int j;
+//		for (j = 0; j < 4; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,0};
+//	int i = 0;
+//	int len = sizeof(arr)/sizeof(arr[0]);
+//	for (i = 0; i < len; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	char arr1[] ="abc";
+//	char arr2[] = {'a','b','c'};
+//	printf("%d\n", sizeof(arr1));
+//	printf("%d\n", sizeof(arr2));
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+//	return 0;
+//}
+ //void hanoi(int n, char a, char b, char c)       //这里代表将a柱子上的盘子借助b柱子移动到c柱子
+ //  {  if (1 == n)                                //如果是一个盘子直接将a柱子上的盘子移动到c
+ //         {
+	//             printf("%c-->%c\n", a, c);
+	//           }
+ //       else
+ //           {
+	//              hanoi(n - 1, a, c, b);         //将a柱子上n-1个盘子借助c柱子，移动到b柱子
+	//              printf("%c-->%c\n", a, c);     //再直接将a柱子上的最后一个盘子移动到c
+	//              hanoi(n - 1, b, a, c);         //然后将b柱子上的n-1个盘子借助a移动到c
+	//           }
+ //  }
+ //int main()
+ //  {  int  n;
+ //      printf("Input the number of diskes:");
+ //      scanf("%d", &n);
+ //      hanoi(n, 'A', 'B', 'C');
+ //      return 0;
+ //  }
 
 
 //int fb(int n)
