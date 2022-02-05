bef57ca24cf28325d5acdddc525571505b0eca61
ZZyuandaren <97601618+ZZyuandaren@users.noreply.github.com>
2022-02-05 22:44:02 +0800

    Add files via upload

--- text.cpp
+++ text.cpp
@@ -1,12 +1,32 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
-#include"add.h"
 int main()
 {
-	int a = 23;
-	int b = 54;
-	int ret= Add(a, b);
-	printf("%d", ret);
+	int a = 3;
+	int b = 5;
+	int c = a | b;
+	printf(" % d % d % d", a, b, c);
 	return 0;
 }
+//int main()
+/*
+	int a = 3;
+	int b = 5;
+	int c = a & b;
+	printf(" % d % d % d", a, b, c);
+	return 0;
+}*/
+//int main()
+//{
+//	int a = 4;
+//	int b =1 << a;
+//	printf("%d %d", a, b);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 16;
+//	int b = a >> 2;
+//	printf("%d %d", a, b);
+//	return 0;
+//}
