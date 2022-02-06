e929d7a4f554e74ba342d817b671b21e6e918ddd
ZZyuandaren <97601618+ZZyuandaren@users.noreply.github.com>
2022-02-06 22:12:05 +0800

    Add files via upload

--- text.cpp
+++ text.cpp
@@ -1,13 +1,72 @@
+#define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 
 int main()
 {
-	int a = 3;
-	int b = 5;
-	int c = a | b;
-	printf(" % d % d % d", a, b, c);
+	int i = 0, a = 1, b = 2, c = 3, d = 4;
+	i = a++ || ++b || d++;
+	printf("a=%d\n b=%d\n c=%d\n d=%d\n", a, b, c, d);
 	return 0;
 }
+//int main()
+//{
+//	int i = 0, a = 1, b = 2, c = 3, d = 4;
+//	i = a++ && ++b && d++;
+//	printf("a=%d\n b=%d\n c=%d\n d=%d\n", a, b, c, d);
+//	return 0;
+//}
+
+//int main()//统计二进制内存中的1个数
+//{
+//	int num= 0;
+//	scanf("%d", &num);
+//	int bj = 1;
+//	int count = 0;
+//	while (bj <= num)
+//	{
+//		int x = bj & num;
+//		if (x != 0)
+//		{
+//			x = 1;
+//		}
+//		bj = 1 << bj;
+//		count = count + x;
+//	}
+//	printf("%d", count);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	a = a ^ b;
+//	printf("%d", a);
+//	b = a ^ b;
+//	printf("%d", b);
+//	a = a ^ b;
+//	printf("%d", a);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 1;
+//	int b = 2;
+//	int c = a | b;
+//	printf(" % d",  c);
+//	 c = a & b;
+//	printf(" % d",  c);
+//	c = a ^ b;
+//	printf("  % d", c);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	int c = a | b;
+//	printf(" % d % d % d", a, b, c);
+//	return 0;
+//}
 //int main()
 /*
 	int a = 3;
