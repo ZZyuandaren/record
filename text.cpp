61aebf521e86ad128571014ff35967bdee27e507
ZZyuandaren <97601618+ZZyuandaren@users.noreply.github.com>
2022-02-16 23:40:16 +0800

    Add files via upload

--- text.cpp
+++ text.cpp
@@ -1,11 +1,57 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 
-int main()
-{
 
-	return 0;
-}
+
+
+//#define TEN 10
+//int main()
+//{
+//	int n = 0;
+//	while (n++ < TEN)
+//	{
+//		printf("%5d\n", n);
+//
+//	}
+//	return 0;
+//}
+
+//void Printf(int arr[], int sz)
+//{
+//	int i;
+//	for (i = 0; i < sz-1; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//}
+//void reverse(int arr[],int sz)
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//	while (left < right)
+//	{
+//		int tem = arr[left];
+//		arr[left] = arr[right];
+//		arr[right] = tem;
+//		left++;
+//		right--;
+//	}
+//}
+//void Init(int arr[])
+//{
+//	
+//}
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	Init(arr);
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	Printf(arr,sz);//打印
+//	reverse(arr,sz);//交换数组元素
+//	Printf(arr,sz);
+//	return 0;
+//}
 //int main()
 //{
 //	int a = 4,b=5,c=9;
